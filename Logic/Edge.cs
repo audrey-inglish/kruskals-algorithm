089c8cf640443c6d9fbe083acfc26a746c45aaf9
Audrey Inglish <audreyinglish@gmail.com>
2024-04-18 21:48:47 -0600

    working on Kruskal

--- Logic/Edge.cs
+++ Logic/Edge.cs
@@ -6,5 +6,11 @@
         public char Destination { get; set; }
         public int Weight { get; set; }
         public string Name { get => $"{Source}" + $"{Destination}"; }
+
+
+        public int CompareTo(Edge other)
+        {
+            return this.Weight - other.Weight;
+        }
     }
 }
---
+++ Logic/Kruskal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    internal class Kruskal
+    {
+        public int NumVertices { get; set; }
+        public int NumEdges { get; set; }
+
+        List<Edge> edges;
+
+        //default constructor could initialize a graph with a single vertice and no edges
+        //but it won't initialize any vertices -- that should be up to the user
+        public Kruskal()
+        {
+            edges = new List<Edge>();
+        }
+
+        public List<Edge> FindMinimumSpanningTree(List<char> vertices, List<Edge> edges)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
